deb81f676eb28c7f5a8c7ed30f00e820b1f0faec
Tony <pp.tony777@gmail.com>
2024-06-09 10:07:45 -0600

    Getting all the real stores on the sidebar

--- src/shared/sidebar/sidebar.component.ts
+++ src/shared/sidebar/sidebar.component.ts
@@ -1,75 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AuthService } from '../../app/services/auth/auth.service';
 import Cookies from 'js-cookie';
 import { Router } from '@angular/router';
 import { User } from '../../core/modules/user.module';
+import { StoreService } from '../../app/services/store/store.service';
+import { Store } from '../../core/modules/stores.module';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
   user: User;
 
   buildZoneItems = [
-    { label: 'build-zone-mty' },
-    { label: 'build-zone-gdl' },
-    { label: 'build-zone-ags' },
-    { label: 'build-zone-cdmx' },
+    { label: '' },
   ];
 
   items: any = [];
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private storeService: StoreService) {
     let userCookie = Cookies.get('user');
+    
+
     try {
       this.user = JSON.parse(userCookie || '{}') as User;
     } catch (error) {
       console.error('Error parsing user cookie:', error);
       this.user = {} as User;
     }
-    if (this.user.role === 'admin') {
-      this.items = [
-        {
-          label: 'Dashboard',
-          icon: 'pi pi-home',
-          command: () => this.dashboardAdmin(),
-        },
-        {
-          label: 'Inventory',
-          icon: 'pi pi-folder',
-          items: this.buildZoneItems.map((item) => ({
-            ...item,
-            command: () => this.navigate('inventory', item.label),
-          })),
-          command: () => this.navigate('inventory', ''),
-        },
-        {
-          label: 'Report',
-          icon: 'pi pi-chart-bar',
-          items: this.buildZoneItems.map((item) => ({
-            ...item,
-            command: () => this.navigate('report', item.label),
-          })),
-          command: () => this.navigate('report', ''),
-        },
-        {
-          label: 'Managers',
-          icon: 'pi pi-users',
-          items: this.buildZoneItems.map((item) => ({
-            ...item,
-            command: () => this.navigate('managers', item.label),
-          })),
-          command: () => this.navigate('managers', ''),
-        },
-        {
-          label: 'Log out',
-          icon: 'pi pi-power-off',
-          command: () => this.logout(),
-        },
-      ];
-    } else if (this.user.role === 'manager') {
+    if (this.user.role === 'manager') {
       this.items = [
         {
           label: 'Dashboard',
@@ -100,6 +61,61 @@ export class SidebarComponent {
     }
   }
 
+  ngOnInit(): void {
+    this.storeService.getAllStores().subscribe(
+      (stores) => {
+        this.buildZoneItems = stores.map((store: Store) => ({ label: store.store_name }));
+        console.log('Stores:', this.buildZoneItems);
+  
+        if (this.user.role === 'admin') {
+          this.items = [
+            {
+              label: 'Dashboard',
+              icon: 'pi pi-home',
+              command: () => this.dashboardAdmin(),
+            },
+            {
+              label: 'Inventory',
+              icon: 'pi pi-folder',
+              items: this.buildZoneItems.map((item) => ({
+                ...item,
+                command: () => this.navigate('inventory', item.label),
+              })),
+              command: () => this.navigate('inventory', ''),
+            },
+            {
+              label: 'Report',
+              icon: 'pi pi-chart-bar',
+              items: this.buildZoneItems.map((item) => ({
+                ...item,
+                command: () => this.navigate('report', item.label),
+              })),
+              command: () => this.navigate('report', ''),
+            },
+            {
+              label: 'Managers',
+              icon: 'pi pi-users',
+              items: this.buildZoneItems.map((item) => ({
+                ...item,
+                command: () => this.navigate('managers', item.label),
+              })),
+              command: () => this.navigate('managers', ''),
+            },
+            {
+              label: 'Log out',
+              icon: 'pi pi-power-off',
+              command: () => this.logout(),
+            },
+          ];
+        }
+        // similar code for other roles
+      },
+      (error) => {
+        console.error('Error fetching stores:', error);
+      }
+    );
+  }
+  
   navigate(page: string, storeName: string): void {
     console.log(`Navigating to ${page} for store: ${storeName}`);
     this.router.navigate([`/${page}`, { storeName: storeName }]);
@@ -116,7 +132,6 @@ export class SidebarComponent {
 
   dashboardManager(): void {
     this.router.navigate(['/dashboard']);
-    console.log('Dashboard Manager');
   }
 
   InventoryManager(): void {
